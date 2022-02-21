e660f20ce0cc5efddf922f833baaa851801bdc43
Simon Martina-Perez <simonmartinaperez@gmail.com>
2022-02-21 21:56:00 +0000

    Add print statement for v_old, v_new

--- main.py
+++ main.py
@@ -178,6 +178,8 @@ class NSSolver:
         #Flow problem variables
         self.str_old.assign(str_new)
         self.p_old.assign(p_new)
+        print("v_old",v_old)
+        print("v_new",v_new)
         self.v_old.assign(v_new)
         self.phi_old.assign(phi_new)       
 
