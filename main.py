3c5e33ad259872751ebf9847b051b911dfc46b49
Simon Martina-Perez <simonmartinaperez@gmail.com>
2022-02-22 09:42:12 +0000

    Changed assigner

--- main.py
+++ main.py
@@ -177,7 +177,10 @@ class NSSolver:
         #Flow problem variables
         self.str_old.assign(str_new)
         self.p_old.assign(p_new)
-        self.v_old.assign(v_new)
+
+        assigner = FunctionAssigner(V,flowspace.sub(0))
+        assigner.assign(self.v_old,vpr_new.sub(0))
+        #self.v_old.assign(v_new)
         self.phi_old.assign(phi_new)       
 
 # Defining the problem
