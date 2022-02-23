752a0032c7ccd9a7f1ec12b1325b7490ce34ded8
Simon Martina-Perez <simonmartinaperez@gmail.com>
2022-02-23 14:44:21 +0000

    removed krylov

--- main.py
+++ main.py
@@ -119,19 +119,14 @@ class NSSolver:
         #POLARITY EVOLUTION #
         #Define variational formulation
         y = TestFunction(V)
-        #Fp = (1./dt)*inner(p_new-p_old,y)*dx - inner(nabla_grad(p_old)*(v_old + w_sa*p_old),y)*dx
+        Fp = (1./dt)*inner(p_new-p_old,y)*dx - inner(nabla_grad(p_old)*(v_old + w_sa*p_old),y)*dx
         #Take functional derivative
-        #J = derivative(Fp,p_new)
+        J = derivative(Fp,p_new)
         #set boundary conditions
         zero = Expression(('0.0','0.0','0.0'),degree=2)
         bcs = DirichletBC(V,zero,self.boundary)
         #Solve variational problem
-        #solve(Fp==0,p_new,J=J,bcs=bcs,solver_parameters={"newton_solver":{"linear_solver" : "superlu_dist"}})
-        linearSolver = PETScKrylovSolver("gmres","jacobi")
-        A = assemble((1./dt)*inner(p_new-p_old,y)*dx)
-        B = assemble(inner(nabla_grad(p_old)*(v_old + w_sa*p_old),y)*dx)
-        bcs.apply(A,B)
-        linearSolver.solve(A,p_new.vector(),B)
+        solve(Fp==0,p_new,J=J,bcs=bcs,solver_parameters={"newton_solver":{"linear_solver" : "superlu_dist"}})
 
         #STRESS TENSOR 
         #Define variational formulation
