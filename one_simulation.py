de0dda729a3399892fe5cb8ea1e17f1a256eec99
Simon Martina-Perez <simonmartinaperez@gmail.com>
2022-03-10 11:53:29 +0000

    Start with nonzero initial guess

--- one_simulation.py
+++ one_simulation.py
@@ -123,12 +123,8 @@ w2 = TestFunction(W)
 
 #Define operator for polarity
 a_pol = (1. / dt) * dot(uV, y) * dx
-# A_pol = assemble(a_pol)
 zero = Expression(('0.0', '0.0', '0.0'), degree=2)  # Expression(('0.0','0.0','0.0'), degree=2)
 bcs_pol = DirichletBC(V, zero, boundary)
-# bcs_pol.apply(A_pol)
-# solver_pol = KrylovSolver("superlu_dist", "ilu")
-# solver_pol.set_operator(A_pol)
 
 #Define operator for stress
 a_str = (1 + eta / (E_bulk * dt)) * inner(uT, z) * dx
@@ -137,36 +133,20 @@ zero = Expression((('0.0', '0.0', '0.0'),
                    ('0.0', '0.0', '0.0'),
                    ('0.0', '0.0', '0.0')), degree=2)
 bcs_str = DirichletBC(TS, zero, boundary)
-# bcs_str.apply(A_str)
-# solver_str = KrylovSolver("superlu_dist", "ilu")
-# solver_str.set_operator(A_str)
 
 #Define operator for flow problem
 a_flow = eta * inner(nabla_grad(du1), nabla_grad(y1)) * dx +\
     dot(nabla_grad(du2), y1) * dx +\
     dot(div(du1), w1) * dx +\
     gamma * dot(du1, y1) * dx
-# A_flow = assemble(a_flow)
 zero = Expression(('0.0', '0.0', '0.0', '0.0'), degree=2)
 bcs_flow = DirichletBC(flowspace, zero, boundary)
-# bcs_flow.apply(A_flow)
-# solver_flow = KrylovSolver("superlu_dist", "ilu")
-# solver_flow.set_operator(A_flow)
 
 #Define operator for phase field
 a_phi = (1. / dt) * uphi * w2 * dx
 # A_phi = assemble(a_phi)
 zero = Expression(('0.0'), degree=2)
 bcs_phi = DirichletBC(W, zero, boundary)
-# bcs_phi.apply(A_phi)
-# solver_phi = KrylovSolver("superlu_dist", "ilu")
-# solver_phi.set_operator(A_phi)
-
-# def E(u):
-#     return sym(nabla_grad(u))
-#
-# def W(u):
-#     return skew(nabla_grad(u))
 
 phi_file = File("results/phi.pvd")
 p_file = File("results/p.pvd")
@@ -180,6 +160,9 @@ phi_file << phi_old
 p_file << p_old
 v_file << v_old
 
+p_new.assign(p_old)
+phi_new.assign(phi_old)
+
 for i in tqdm(range(numSteps)):
     print('Initializing i=', i)
     print('polarity', p_old.vector().get_local().min(), p_old.vector().get_local().max())
@@ -188,43 +171,22 @@ for i in tqdm(range(numSteps)):
 
     # POLARITY EVOLUTION #
     L_pol = (1. / dt) * dot(p_old, y) * dx - dot(nabla_grad(p_old) * (v_old + w_sa * p_old), y) * dx
-    # if i==0:
-    #     b_pol = assemble(L_pol)
-    # else:
-    #     b_pol = assemble(L_pol, tensor=b_pol)
     solve(a_pol == L_pol, p_new, bcs_pol, solver_parameters=dict(linear_solver='gmres',
                                                                  preconditioner='ilu'))
-    #solver_pol.solve(p_new.vector(), b_pol)
 
     # STRESS TENSOR
     L_str = eta * inner(sym(nabla_grad(v_old)), z) * dx + (eta / E_bulk * dt) * inner(str_old, z) * dx
-    # if i==0:
-    #     b_str = assemble(L_str)
-    # else:
-    #     b_str = assemble(L_str, tensor=b_str)
     solve(a_str == L_str, str_new, bcs=bcs_str, solver_parameters=dict(linear_solver='gmres',
                                                                  preconditioner='ilu'))
-    # solver_str.solve(str_new.vector(), b_str)
 
     # FLOW PROBLEM#
     L_flow = - zeta * dot(div(outer(p_new, p_new)), y1) * dx
-    # if i==0:
-    #     b_flow = assemble(L_flow)
-    # else:
-    #     b_flow = assemble(L_flow, tensor=b_flow)
-    # print('flow', abs(np.linalg.eig(A_flow.array())[0]).min())
-    # solver_flow.solve(vpr_new.vector(), b_flow)
     solve(a_flow == L_flow, vpr_new, bcs_flow, solver_parameters=dict(linear_solver='gmres',
                                                                  preconditioner='ilu'))
     # PHASE FIELD PROBLEM#
     L_phi = (1. / dt) * phi_old * w2 * dx + dot(v_new, nabla_grad(phi_old)) * w2 * dx
-    # if i==0:
-    #     b_phi = assemble(L_phi)
-    # else:
-    #     b_phi = assemble(L_phi, tensor=b_phi)
     solve(a_phi == L_phi, phi_new, bcs_phi, solver_parameters=dict(linear_solver='gmres',
                                                                       preconditioner='ilu'))
-    # solver_phi.solve(phi_new.vector(), b_phi)
 
     # ASSIGN ALL VARIABLES FOR NEW STEP
     str_old.assign(str_new)
